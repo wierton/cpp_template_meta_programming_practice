7258ec5b0a705375b0b1457dd2e801df015bc377
141242068-ouxianfei <141242068@smail.nju.edu.cn>
2017-09-04 10:07:49 +0800

    improve the codes of replace_type

--- practice_2.cpp
+++ practice_2.cpp
@@ -32,6 +32,8 @@ void test_add_const_ref() {
  * replace type x by y for all x appears in c
  * note: you can limit the number of function arguments to 2
  * */
+
+/* preset declaration */
 template<class T, class X, class Y, bool cond>
 struct replace_type_impl {};
 
@@ -40,44 +42,64 @@ struct replace_type {
 	typedef typename replace_type_impl<T, X, Y, is_same<T, X>::value>::type type;
 };
 
+/* the same type */
 template<class T, class X, class Y>
 struct replace_type_impl<T, X, Y, true> {
 	typedef Y type;
 };
 
+/* pointer type */
 template<class T, class X, class Y>
 struct replace_type_impl<T *, X, Y, false> {
 	typedef typename replace_type<T, X, Y>::type * type;
 };
 
+/* lvalue reference type */
 template<class T, class X, class Y>
 struct replace_type_impl<T &, X, Y, false> {
 	typedef typename replace_type<T, X, Y>::type & type;
 };
 
+/* rvalue reference type */
 template<class T, class X, class Y>
 struct replace_type_impl<T &&, X, Y, false> {
 	typedef typename replace_type<T, X, Y>::type && type;
 };
 
+/* null array type */
 template<class T, class X, class Y>
 struct replace_type_impl<T [], X, Y, false> {
 	typedef typename replace_type<T, X, Y>::type type[];
 };
 
+/* fixed length array type */
 template<class T, class X, class Y, unsigned N>
 struct replace_type_impl<T [N], X, Y, false> {
 	typedef typename replace_type<T, X, Y>::type type[N];
 };
 
+/* function pointer type, 1 arguments */
 template<class T, class X, class Y, class V>
 struct replace_type_impl<T (*)(V), X, Y, false> {
 	typedef typename replace_type<T, X, Y>::type (*type) (typename replace_type<V, X, Y>::type);
 };
 
+/* function pointer type, 2 arguments */
 template<class T, class X, class Y, class V1, class V2>
 struct replace_type_impl<T (*)(V1, V2), X, Y, false> {
 	typedef typename replace_type<T, X, Y>::type (*type) (typename replace_type<V1, X, Y>::type, typename replace_type<V2, X, Y>::type);
+};
+
+/* function type, 1 arguments */
+template<class T, class X, class Y, class V>
+struct replace_type_impl<T (V), X, Y, false> {
+	typedef typename replace_type<T, X, Y>::type type (typename replace_type<V, X, Y>::type);
+};
+
+/* function type, 2 arguments */
+template<class T, class X, class Y, class V1, class V2>
+struct replace_type_impl<T (V1, V2), X, Y, false> {
+	typedef typename replace_type<T, X, Y>::type type (typename replace_type<V1, X, Y>::type, typename replace_type<V2, X, Y>::type);
 };
 
 void test_replace_type() {
@@ -85,7 +107,13 @@ void test_replace_type() {
 	static_assert(is_same<replace_type<int [], int, char const>::type, const char []>::value, "int [] --> const char []");
 	static_assert(is_same<replace_type<int [4], int, char const>::type, const char [4]>::value, "int [4] --> const char [4]");
 	static_assert(is_same<replace_type<int (*)(int, int), int, char const>::type, const char (*)(const char, const char)>::value, "int (*)(int, int) --> const char (*)(const char, const char)");
+	static_assert(is_same<replace_type<int (int, int), int, char const>::type, const char (const char, const char)>::value, "int (int, int) --> const char (const char, const char)");
 }
+
+
+/*
+ *
+ */
 
 int main() {
 	test_add_const_ref();
